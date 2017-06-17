3bb2cb455ad8096c0aac94ea52717974e2412a71
Joshua Lee <jdl93@Joshuas-MacBook-Air.local>
2017-06-16 18:08:48 -1000

    changes to js

--- public/js/app.js
+++ public/js/app.js
@@ -18,47 +18,55 @@ function redditData(){
   //console.log('*** this is result.data', result.data);
   //console.log('*** this is result.children', result.data.children);
 
-  for (var i = 0; i < result.data.children.length; i++){
-    console.log('*** this is result after the loop', result.data.children[i]);
-    if (result.data.children[i].data.post_hint === 'image'){
-    //console.log(result.data.children[i].data.post_hint);
-    //console.log(result.data.children[i].data.url);
-    //this is the container for the boards
+   for (var i = 0; i < result.data.children.length; i++){
+     console.log('*** this is result after the loop', result.data.children[i]);
+     if (result.data.children[i].data.post_hint === 'image'){
+     //console.log(result.data.children[i].data.post_hint);
+     //console.log(result.data.children[i].data.url);
+     //this is the container for the boards
 
-    //This is the main div where all the data is placed
-    var boardContainer = document.createElement('div');
-    boardContainer.className = 'boards';
-    main.appendChild(boardContainer);
+     //This is the main div where all the data is placed
+     var boardContainer = document.createElement('div');
+     boardContainer.className = 'boards';
+     main.appendChild(boardContainer);
 
-    //This is the the image
-    var imageContainer = document.createElement('div');
-    imageContainer.className = 'imageContainer';
-    imageContainer.src = result.data.children[i].data.url;
-    boardContainer.appendChild(imageContainer);
+     //This is the the image
+     var imageContainer = document.createElement('div');
+     imageContainer.className = 'imageContainer';
+     imageContainer.src = result.data.children[i].data.url;
+     boardContainer.appendChild(imageContainer);
 
-    //This is the the image
-    var boardImage = document.createElement('img');
-    boardImage.className = 'image';
-    boardImage.src = result.data.children[i].data.url;
-    imageContainer.appendChild(boardImage);
+     //This is the the image
+     var boardImage = document.createElement('img');
+     boardImage.className = 'image';
+     boardImage.src = result.data.children[i].data.url;
+     imageContainer.appendChild(boardImage);
 
-    //This is the container for the title
-    var boardTitle = document.createElement('div');
-    boardTitle.className = 'title';
-    boardTitle.innerHTML = result.data.children[i].data.title;
-    boardContainer.appendChild(boardTitle);
+     //This is the container for the title
+     var boardTitle = document.createElement('div');
+     boardTitle.className = 'title';
+     boardTitle.innerHTML = result.data.children[i].data.title;
+     boardContainer.appendChild(boardTitle);
 
-    //This is the container for the author
-    var boardAuthor = document.createElement('div');
-    boardAuthor.className = 'author';
-    boardAuthor.innerHTML = 'By' + ' ' + result.data.children[i].data.author;
-    boardContainer.appendChild(boardAuthor);
+     //This is the container for the author and views
+     var boardData = document.createElement('div');
+     boardData.className = 'data';
+     boardData.innerHTML = 'By' + ' ' + result.data.children[i].data.author + ' - ' + 'Views' + ' ' + result.data.children[i].data.score;
+     boardContainer.appendChild(boardData);
 
-    //this is the container for the text
-    // var boardtext = document.createElement ('p');
-    // boardtext.className = 'text';
-    // boardtext.innerHTML = result.data.children[i].data.selftext;
-    // boardContainer.appendChild(boardtext);
+     var dateContainer = document.createElement('div');
+     dateContainer.className = 'date';
+     var timeStamp = new Date(result.data.children[i].data.created * 1000);
+     var hours = timeStamp.getHours();
+     dateContainer.innerHTML = hours + ' '+ 'hours ago';
+     boardContainer.appendChild(dateContainer);
+
+     var text = document.createElement('div');
+     text.className = 'text';
+     text.innerHTML = 'subreddit:' + ' ' + result.data.children[i].data.subreddit;
+     boardContainer.appendChild(text);
+
     }
   }
 }
+
